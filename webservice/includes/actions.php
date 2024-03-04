e19d938a2de3ccfb261723dcd5e77f1aa27f3d85
Alana le Feber <alana.l.f@upcmail.nl>
2024-03-04 16:41:06 +0100

    css passend gemaakt

--- webservice/includes/actions.php
+++ webservice/includes/actions.php
@@ -7,29 +7,54 @@ function getBooks()
     return [
         [
             "id" => 1,
-            "name" => "Pizza",
-            "kitchen" => "Italian",
+            "title" => "Legends and Lattes",
+            "author" => "Travis Baldree",
         ],
         [
             "id" => 2,
-            "name" => "Kale",
-            "kitchen" => "Dutch",
+            "title" => "A Deadly Education",
+            "author" => "Naomi Novik",
         ],
         [
             "id" => 3,
-            "name" => "Lasagna",
-            "kitchen" => "Italian",
+            "title" => "Good Omens",
+            "author" => "Terry Pratchet & Neil Gaiman",
         ],
         [
             "id" => 4,
-            "name" => "Kebab",
-            "kitchen" => "Turkish",
+            "title" => "Six of Crows",
+            "author" => "Leigh Bardugo",
         ],
         [
             "id" => 5,
-            "name" => "Paella",
-            "kitchen" => "Spanish",
-        ]
+            "title" => "The Midnight Library",
+            "author" => "Matt Haig",
+        ],
+        [
+            "id" => 6,
+            "title" => "They Both Die at the End",
+            "author" => "Adam Silvera",
+        ],
+        [
+            "id" => 7,
+            "title" => "The Song of Achilles",
+            "author" => "Madeline Miller",
+        ],
+        [
+            "id" => 8,
+            "title" => "Circe",
+            "author" => "Madeline Miller",
+        ],
+        [
+            "id" => 9,
+            "title" => "A Game of Thrones",
+            "author" => "George R.R. Martin",
+        ],
+        [
+            "id" => 10,
+            "title" => "The Chalice of the Gods",
+            "author" => "Rick Riordan",
+        ],
     ];
 }
 
--- webservice/index.php
+++ webservice/index.php
@@ -13,3 +13,71 @@ if (!isset($_GET['id'])) {
 header("Content-Type: application/json");
 echo json_encode($data);
 exit;
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<header>
+    <h1>Book Magazine</h1>
+</header>
+
+<main>
+
+    <section id="book-column">
+        <div class="book-box">
+            <h3>Legends and Lattes</h3>
+            <div class="book-img-box">
+                    <img src="./images/percy-jackson.jpg" class="book-img">
+            </div>
+            <button>Add to favorites</button>
+            <button>Show book</button>
+        </div>
+
+        <div class="book-box">
+            <h3>A Deadly Education</h3>
+            <div class="book-img-box">
+                <img src="./images/percy-jackson.jpg" class="book-img">
+            </div>
+            <button>Add to favorites</button>
+            <button>Show book</button>
+        </div>
+
+        <div class="book-box">
+            <h3>Good Omens</h3>
+            <div class="book-img-box">
+                <img src="./images/percy-jackson.jpg" class="book-img">
+            </div>
+            <button>Add to favorites</button>
+            <button>Show book</button>
+        </div>
+
+        <div class="book-box">
+            <h3>Six of Crows</h3>
+            <div class="book-img-box">
+                <img src="./images/percy-jackson.jpg" class="book-img">
+            </div>
+            <button>Add to favorites</button>
+            <button>Show book</button>
+        </div>
+    </section>
+
+</main>
+
+<footer>
+
+</footer>
+
+
+</body>
+</html>
